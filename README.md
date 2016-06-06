7eb063542ecbe60878024221a21c7edc939f716d
Fayland Lam <fayland@gmail.com>
2016-06-06 12:21:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Mojo::UserAgent::CookieJar::ChromeMacOS - Blah blah blah
+
+# SYNOPSIS
+
+    use Mojo::UserAgent::CookieJar::ChromeMacOS;
+
+# DESCRIPTION
+
+Mojo::UserAgent::CookieJar::ChromeMacOS is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Mojo/UserAgent/CookieJar/ChromeMacOS.pm
@@ -0,0 +1,39 @@
+package Mojo::UserAgent::CookieJar::ChromeMacOS;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::UserAgent::CookieJar::ChromeMacOS - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Mojo::UserAgent::CookieJar::ChromeMacOS;
+
+=head1 DESCRIPTION
+
+Mojo::UserAgent::CookieJar::ChromeMacOS is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
